852a4bfb5207d9c706c6b3a2314e34be1293e04f
Vijay Sharma <murlivala.developer@gmail.com>
2017-08-13 15:23:27 +1000

    Added 'Refresh' menu option to download available images for visible screen if not already downloaded by 'Swipe'

--- app/src/main/java/mock/newsfeed/NewsFeedActivity.java
+++ app/src/main/java/mock/newsfeed/NewsFeedActivity.java
@@ -4,6 +4,8 @@ import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -76,6 +78,32 @@ public class NewsFeedActivity extends AppCompatActivity implements ResponseCallb
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            imageUtils = new ImageUtils(NewsFeedActivity.this,
+                    firstVisible,totalVisible);
+            imageUtils.setResponseCallback(NewsFeedActivity.this);
+            imageUtils.execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
